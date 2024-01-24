69c0206fe8e66a3848c3d41c2f0cf2cc5a292f16
Sourav <sourav.singh@emoha.com>
2024-01-24 08:58:45 +0530

    rewamp

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Index = () => (
 	<Page>
 		<Section>
 			<h2 className='text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
-				PWA APP in progress....
+				PWA APP in progress.... contact developer Sourav for more info
 			</h2>
 
 			<h1>PWA APP FOR TESTING ONLY ________</h1>
